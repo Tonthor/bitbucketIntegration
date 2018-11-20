1a8684d792b15b2cce04c46f8c3cad7fad3f7526
Tonthor <starmarv@googlemail.com>
2018-11-20 13:29:18 +0100

    The initial setup, by the Moodle Doc

---
+++ lang/en/repository_bitbucketintegration.php
@@ -0,0 +1,5 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$string['configplugin'] = 'Configuration for my Bitbucket Plugin';
+$string['pluginname'] = 'bitbucketintegration';
+$string['pluginname_help'] = 'Aims to integrate bitbucket functionallity into Moodle to properly track versioning';
